7082dc13340bacf6acfc2d725b2f922f90d6d249
Luchi <ftoluchi@gmail.com>
2026-03-18 13:30:57 +0100

    day three

---
+++ Javascript/Gate-Three/TaskOne.js
@@ -0,0 +1,52 @@
+//
+//
+//
+//function subtract_length(){
+//
+//    let numbers = [10,11,12];
+//
+//    numbers.pop(-1);
+//
+//    numbers.pop(-1);
+//
+//    console.pop(numbers);
+// 
+//}
+//subtract_length()
+//
+//
+
+function add_length(){
+
+    let numbers = [10,11,12];
+
+    numbers.push(-1);
+
+    numbers.push(-1);
+
+    console.log(numbers);
+ 
+}
+add_length()
+
+
+
+
+
+function AddToLength(){
+
+    let numbers = [ ];
+
+    numbers.push(-1);
+
+    numbers.push(-1);
+
+    numbers.push(-1);
+
+    numbers.push(-1);
+
+    console.log(numbers);
+ 
+}
+AddToLength()
+
